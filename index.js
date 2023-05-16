14dc9e34502560fcb879ce31624593e2e2f4e1ce
Motty-har <harligmotty@gmail.com>
2023-05-16 00:13:15 -0400

    Completed

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  for (let i = 0; i < word.length / 2; i++){
+      const j = word.length - 1 - i;
+      if(word[i] !== word[j]){
+        return false
+      }
+  }
+  return true
 }
+  /*Add your pseudocode here
+    ieterate through the string from begining untill the middle
+     compare what we iterate through with the letter at the end of 
+     the string
+     if letters dont match return false
+     if we reach the middle and all letters match return true 
+
+*/
+
 
 /* 
-  Add your pseudocode here
-*/
+ 
+  
+
+  // Write your algorithm here
+  we need to make a function that recieves a string as an argument 
+  and compares the first letter and last letter to see if thei matching.
+  if each of the corresponding letters match untill they meet up on the 
+  middle then will return true, if not will return false.
+  */
 
 /*
   Add written explanation of your solution here
